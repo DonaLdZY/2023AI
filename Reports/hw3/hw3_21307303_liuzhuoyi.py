372ff09f117c93880922cff8ff13969cf3998568
DonaLdZY <760463769@qq.com>
2023-03-08 09:39:40 +0800

    hw3

--- Reports/hw3/hw3_21307303_liuzhuoyi.py
+++ Reports/hw3/hw3_21307303_liuzhuoyi.py
@@ -17,6 +17,10 @@ class task1:
                 ans.append(i)
         return tuple(ans)
 def ResolutionProp(KB):
+    """
+    :param KB: set(tuple(str))
+    :return: list[str]
+    """
     ans=[]
     a=list(KB)
     ai=len(a)
@@ -40,44 +44,39 @@ def ResolutionProp(KB):
             j+=1
     return ans
 
-def decoder(f:str): #将原子公式拆解成谓词名与项
+
+def diff(f1:list,f2:list): #找出不匹配项
+    for i in range(len(f1)):
+        if f1[i]!=f2[i]:
+            return (f1[i],f2[i])
+def isvariate(f:str): #判断是否是变量
+    if f in ['xx','yy','zz','uu','vv','ww']:
+        return True
+    return False
+def peel(f:str): #去掉最外层谓词
     nl=0
-    while nl<len(f) and f[nl].isalpha():
+    while nl<len(f) and (f[nl].isalpha() or f[nl]=='~'):
         nl+=1
     name=f[0:nl]
     if nl<len(f):
         f=f[nl+1:len(f)]
         f=f[0:len(f)-1]
-        items=f.split(',')
     else:
-        items=[]
-    return (name,items)
-def diff(f1:list,f2:list):
-    for i in range(len(f1)):
-        if f1[i]!=f2[i]:
-            return (f1[i],f2[i])
-def peel(f:str):
-    nl=0
-    while nl<len(f) and f[nl].isalpha():
-        nl+=1
-    name=f[0:nl]
-    f=f[nl+1:len(f)]
-    f=f[0:len(f)-1]
+        f=""
     return (name,f)
-def peels(f1:str,f2:str):
-    while (f1[-1]==')' and f2[-1]==')'):
-        (temp,f1)=peel(f1)
-        (temp,f2)=peel(f2)
-    return (f1,f2)
-def isvariate(f:str):
-    if f in ['xx','yy','zz','uu','vv','ww']:
-        return True
-    return False
 def MGU(f1:str, f2:str):
-    (f1name,f1item)=decoder(f1)
-    (f2name,f2item)=decoder(f2)
+    """
+    :param f1: str
+    :param f2: str
+    :return: dict
+    """
+    (f1name,f1in)=peel(f1)
+    (f2name,f2in)=peel(f2)
+    if (f1name != f2name):
+        return {}
+    f1item=f1in.split(',')
+    f2item=f2in.split(',')
     ans={}
-    k=0
     while (f1item != f2item):
         (f1x,f2x)=diff(f1item,f2item)
         while (f1x[-1]==')' and f2x[-1]==')'):
@@ -93,6 +92,7 @@ def MGU(f1:str, f2:str):
             while i<len(f1item):
                 f1item[i]=f1item[i].replace(f1x,f2x)
                 i+=1
+            #还得改字典
         elif (isvariate(f2x) and (not f2x in f1x)):
             if f2x in ans:
                 return {}
@@ -104,14 +104,76 @@ def MGU(f1:str, f2:str):
         else:
             return{}
     return ans
-
-
+def check(a:tuple,b:tuple):
+    if (len(a)>1 and len(b)>1):
+        return ("",{},2)
+    for ai in a:
+        (ainame,aitem)=peel(ai)
+        for bi in b:
+            (biname,bitem)=peel(bi)
+            if (ainame=='~'+biname):
+                if (ai=='~'+bi):
+                    return (bi,{},0)
+                chg=MGU(ai[1:len(ai)],bi)
+                if (chg!={}):
+                    return (bi,chg,0)
+            if ('~'+ainame==biname):
+                if ('~'+ai==bi):
+                    return (ai,{},1)
+                chg=MGU(ai,bi[1:len(bi)])
+                if (chg!={}):
+                    return (ai,chg,1)
+    return ("",{},2)
+def chgs(a:str,chg:dict):
+    for (key,val) in chg.items():
+        #print("changing",key,val)
+        a=a.replace(key,val)
+    return a
+def fusion(a:tuple,b:tuple,key:str,chg:dict):
+    key=chgs(key,chg)
+    ans=[]
+    for j in a:
+        i=chgs(j,chg)
+        if i!=key:
+            ans.append(i)
+    for j in b:
+        i=chgs(j,chg)
+        if i!='~'+key:
+            ans.append(i)
+    return tuple(ans)
 def ResolutionFOL(KB):
-    """
-    :param KB: set(tuple(str))
-    :return: list[str]
-    """
-    return
+    ans=[]
+    a=list(KB)
+    ai=len(a)
+    ais=ai
+    for i in range(ais):
+        ans.append(str(i+1)+' '+str(a[i]))
+    i=0
+    while i<ai:
+        j=i+1
+        while j<ai:
+            (key,chg,fr)=check(a[i],a[j])
+            if (fr!=2):
+                aadd=()
+                if fr:
+                    aadd=fusion(a[i],a[j],key,chg)
+                else:
+                    aadd=fusion(a[j],a[i],key,chg)
+                #print(a[i],a[j],'=>',fr,key,chg,aadd)
+                if (aadd in a):
+                    j+=1
+                    continue
+                a.append(aadd)
+                ai+=1
+                if (chg=={}):
+                    ans.append(str(ai)+' R['+str(i+1)+','+str(j+1)+']: '+str(aadd))
+                else:
+                    ans.append(str(ai)+' R['+str(i+1)+','+str(j+1)+']'+str(chg)+':'+str(aadd))
+                if aadd==():
+                    return ans
+            j+=1
+        i=i+1
+    return ans
 
 
 if __name__ == '__main__':
@@ -124,7 +186,7 @@ if __name__ == '__main__':
     print(MGU('P(xx,a)', 'P(b,yy)'))
     print(MGU('P(a,xx,f(g(yy)))', 'P(zz,f(zz),f(uu))'))
 
-    # KB2 = {('On(a,b)',), ('On(b,c)',), ('Green(a)',), ('~Green(c)',), ('~On(xx,yy)', '~Green(xx)', 'Green(yy)')}
-    # result2 = ResolutionFOL(KB2)
-    # for r in result2:
-    #     print(r)
+    KB2 = {('On(a,b)',), ('On(b,c)',), ('Green(a)',), ('~Green(c)',), ('~On(xx,yy)', '~Green(xx)', 'Green(yy)')}
+    result2 = ResolutionFOL(KB2)
+    for r in result2:
+        print(r)
