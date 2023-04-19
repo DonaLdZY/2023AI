fa8abdfd1c09bc633e9b96053386d9173c221574
DonaLdZY <760463769@qq.com>
2023-04-19 23:25:29 +0800

    hw7finish

--- Reports/hw7/draw.py
+++ Reports/hw7/draw.py
@@ -8,15 +8,13 @@ if __name__=='__main__':
     with open('hw7_21307303_liuzhuoyi_log.txt','r')as ip:
         for line in ip.readlines():
             datas.append(line.rstrip().split(' '))
-    for i in datas:
-        print(i)
     x=[float(i[0]) for i in datas]
     tl=[float(i[1]) for i in datas]
     ta=[float(i[2]) for i in datas]
     vl=[float(i[3]) for i in datas]
     va=[float(i[4]) for i in datas]     
     datax=[]
-    with open('hw7_21307303_liuzhuoyi_test_log.txt',"r")as ip:
+    with open('hw7_21307303_liuzhuoyi_model-score.txt',"r")as ip:
          for line in ip.readlines():
             datax.append(line.rstrip().split(' '))
     x2=[float(i[0]) for i in datax]
@@ -24,36 +22,19 @@ if __name__=='__main__':
     plt.clf()
     plt.plot(x[1:],ta[1:],color='g')
     plt.plot(x[1:],va[1:],color='b')  
-    for i in range(1,len(s)):
+    for i in range(2,len(s)):
         plt.plot([x2[i-1],x2[i]],[s[i-1],s[i-1]],color='r')
         plt.plot([x2[i],x2[i]],[s[i-1],s[i]],color='r')
     plt.plot([x2[-1],x[-1]],[s[-1],s[-1]],color='r')
-    plt.title("accuracy")
+    plt.title("valid_acc="+str(va[-1])+"  test_acc="+str(s[-1]))
+    plt.xlabel("epochs")
+    plt.ylabel("acc")
     plt.savefig("accuracy.jpg")
+
     plt.clf()
     plt.plot(x[1:],tl[1:],color='g')
-    plt.plot(x[1:],vl[1:],color='b')     
+    plt.plot(x[1:],vl[1:],color='b')   
+    plt.xlabel("epochs")  
+    plt.ylabel("loss")
     plt.title("loss")
     plt.savefig("loss.jpg")
-# def drawmap(self): #绘制当前路线
-#         plt.clf()
-#         xs=[self.city_location[i][0] for i in self.population[0][0:len(self.population[0])-1]]
-#         ys=[self.city_location[i][1] for i in self.population[0][0:len(self.population[0])-1]]
-#         xs.append(xs[0])
-#         ys.append(ys[0])
-#         plt.plot(xs,ys,color='b')
-#         plt.scatter(xs,ys,color='r',s=10)
-#         plt.title('gen '+str(self.gen)+'\nShortest Path: '+str(self.best))
-#         plt.savefig(self.log_path+str(self.data_name)+'_gen'+str(self.gen)+'.jpg')
-#     def plot_diagram(self): #绘制数据迭代表
-#         plt.clf()
-#         # for i in range(len(self.log_point)):
-#         #     if self.max_log[i]!=self.min_log[i]:
-#         #         plt.plot([self.log_point[i], self.log_point[i]], [self.max_log[i],self.min_log[i]], color='b')
-#         for i in range(1,len(self.log_point)):
-#             plt.plot([self.log_point[i-1], self.log_point[i]], [self.max_log[i-1],self.max_log[i]], color='r')
-#         plt.title('gen '+str(self.gen)+'\nShortest Path: '+str(self.best))
-#         plt.ylabel('cost')
-#         plt.xlabel('iteration')
-#         plt.savefig(self.log_path+str(self.data_name)+'_overall'+'.jpg')
-    
--- Reports/hw7/hw7_21307303_liuzhuoyi.py
+++ Reports/hw7/hw7_21307303_liuzhuoyi.py
@@ -1,11 +1,17 @@
 _exp_name="hw7_21307303_liuzhuoyi"
-#------ import ------
+#------ import env ------
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.distributions import Categorical
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, Dataset, random_split
+
+#确保每次调用卷积算法返回确定性输出，即默认算法
+torch.backends.cudnn.deterministic = True 
+#固定网络结构的模型优化以提高效率，否则会花费时间在选择最合适算法上
+torch.backends.cudnn.benchmark = False 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 #------ Dataset ------
@@ -15,20 +21,16 @@ class MyDataset(Dataset):
             return 
         self.datas=torch.load(datas_dir)
         self.label=torch.load(label_dir)
-
     def __len__(self):
         return self.label.size()[0]
-    
     def __getitem__(self,idx):
         return self.datas[idx],self.label[idx]
 #------ Dataloader ------
-n_workers=2 #用于数据加载的子进程数
-
-def get_dataloader(datas_dir,label_dir, batch_size, n_workers,valid_sept=0):
+def get_dataloader(datas_dir,label_dir,sept=0):
     dataset=MyDataset(datas_dir,label_dir)
-    if valid_sept>0:
+    if sept>0:
         #分割成trainset与validset
-        validlen=int(valid_sept*len(dataset))
+        validlen=int(sept*len(dataset))
         lengths=[len(dataset)-validlen,validlen]
         trainset,validset=random_split(dataset,lengths)
         train_loader=DataLoader(
@@ -52,39 +54,35 @@ def get_dataloader(datas_dir,label_dir, batch_size, n_workers,valid_sept=0):
         loader=DataLoader(
             dataset,
             batch_size=batch_size,
-            shuffle=False, 
+            shuffle=True, 
             num_workers=n_workers, 
             drop_last=True,
             pin_memory=True, 
         )
         return loader
+    
 #------ Model ------
-#确保每次调用卷积算法返回确定性输出，即默认算法
-torch.backends.cudnn.deterministic = True 
-#固定网络结构的模型优化以提高效率，否则会花费时间在选择最合适算法上
-torch.backends.cudnn.benchmark = False 
-
 class MyConvNet(nn.Module):
     def __init__(self):
         super().__init__()
         #input [1,28,28]
         self.cnn=nn.Sequential(
-            nn.Conv2d(1,16,3,1,1), #[16,28,28]
-            nn.BatchNorm2d(16),
+            nn.Conv2d(1,64,3,1,1), #[4,28,28]
+            nn.BatchNorm2d(64),
             nn.ReLU(),
-            nn.MaxPool2d(2,2,0), #[16,14,14]
-
-            nn.Conv2d(16,32,3,1,1), #[32,14,14]
-            nn.BatchNorm2d(32),
+            nn.MaxPool2d(2,2,0), #[64,14,14]
+            
+            nn.Conv2d(64,128,3,1,1), #[128,14,14]
+            nn.BatchNorm2d(128),
             nn.ReLU(),
-            nn.MaxPool2d(2,2,0), #[32,7,7]
+            nn.MaxPool2d(2,2,0), #[128,7,7]
         )
         self.fc=nn.Sequential(
-            nn.Linear(32*7*7,8*7*7),
+            nn.Linear(128*7*7,16*7*7),
             nn.ReLU(),
-            nn.Linear(8*7*7,4*7*7),
+            nn.Linear(16*7*7,2*7*7),
             nn.ReLU(),
-            nn.Linear(4*7*7,10),
+            nn.Linear(2*7*7,10)
         )
     def forward(self, x):
         if len(x.size())==3:
@@ -93,13 +91,6 @@ class MyConvNet(nn.Module):
         out=out.view(out.size()[0],-1)
         out=self.fc(out)
         return torch.softmax(out,dim=-1)
-
-#------ hyper parameter ------
-batch_size=16
-n_epochs=32
-patience=8
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 #------ test ------
 def test(data, labels, net):
     num_data = data.shape[0]
@@ -112,7 +103,6 @@ def test(data, labels, net):
         true_label = labels[i].item()
         if label == true_label:
             num_correct += 1
-
     return num_correct / num_data
 def Testing():
     net= MyConvNet()
@@ -120,7 +110,16 @@ def Testing():
     test_labels = torch.load('data\\hw7\\test_labels.pth')
     net.load_state_dict(torch.load('hw7_21307303_liuzhuoyi.pth'))
     return test(test_data, test_labels, net)
-
+#------ hyper parameter for training------
+n_epochs=64
+patience=24
+log_open=True
+#------ filename ------
+modelfile='hw7_21307303_liuzhuoyi.pth'
+logfile=f"./hw7_21307303_liuzhuoyi_log.txt"
+scorefile=f"./hw7_21307303_liuzhuoyi_model-score.txt"
+train_datafile='data\\hw7\\train_data.pth'
+train_labelfile='data\\hw7\\train_labels.pth'
 #------ main ------
 if __name__=="__main__":
     from tqdm.auto import tqdm #进度条可视化
@@ -131,43 +130,40 @@ if __name__=="__main__":
     #Adam 动态学习率(加快收敛速度)+惯性梯度(避免local minimal)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.0003, weight_decay=1e-5)
 
+    if log_open:
+        with open(logfile,"w")as op:
+            op.write("0 0 0 0 0\n")
+        with open(scorefile,"w")as op:
+            op.write("0 0\n")
+
+    #dataset/dataloader实例化
+    train_loader,valid_loader=get_dataloader(
+        datas_dir=train_datafile,
+        label_dir=train_labelfile,
+        sept=0.167
+    )
     best_acc=0
-    with open(f"./{_exp_name}_log.txt","w")as op:
-        op.write("0 0 0 0 0\n")
+
     for epoch in range(n_epochs):
-        #dataset/dataloader实例化
-        train_loader,valid_loader=get_dataloader(
-            datas_dir='data\\hw7\\train_data.pth',
-            label_dir='data\\hw7\\train_labels.pth',
-            batch_size=batch_size, 
-            n_workers=n_workers,
-            valid_sept=0.1
-        )
-        test_loader=get_dataloader(
-            datas_dir='data\\hw7\\test_data.pth',
-            label_dir='data\\hw7\\test_labels.pth',
-            batch_size=batch_size, 
-            n_workers=n_workers,
-        )
         #----- Training ------
-        #训练模式
+        #开启梯度信息
         model.train() 
         #训练记录
         train_loss=[]
         train_accs=[]
+
         for batch in tqdm(train_loader):
-            #数据
+            #获取数据
             imgs,labels=batch
             #模型预测
-            # imgs.size()=torch.Size([16, 1, 28, 28])
             logits=model(imgs.to(device))
-            #计算损失
+            #计算损失函数
             loss=criterion(logits,labels.to(device))
-            #梯度归零
+            #梯度清零
             optimizer.zero_grad()
-            #反向传播 计算梯度
+            #反向传播，计算梯度
             loss.backward()
-            # Clip the gradient norms for stable training.
+            #梯度裁剪，防止梯度爆炸
             grad_norm = nn.utils.clip_grad_norm_(model.parameters(), max_norm=10)
             #更新参数
             optimizer.step()
@@ -176,52 +172,51 @@ if __name__=="__main__":
             train_loss.append(loss.item())
             train_accs.append(acc)
 
+        #求平均损失与准确率
         train_loss = sum(train_loss) / len(train_loss)
         train_acc = sum(train_accs) / len(train_accs)
-        # 输出信息
         print(f"[ Train | {epoch + 1:03d}/{n_epochs:03d} ] loss = {train_loss:.5f}, acc = {train_acc:.5f}")
 
-        with open(f"./{_exp_name}_log.txt","a") as op:
-            op.write(f"{epoch + 1:03d} {train_loss:.5f} {train_acc:.5f} ")
         # ------ Validation ------
-        #无梯度信息模式
+        #关闭梯度信息
         model.eval()
-        #信息记录
+        #验证记录
         valid_loss=[]
         valid_accs=[]
+
         for batch in tqdm(valid_loader):
+            #获取数据
             imgs, labels = batch
             # 验证时不需要计算梯度
             with torch.no_grad():
                 logits = model(imgs.to(device))
             #求损失函数
             loss = criterion(logits, labels.to(device))
-            # 求准确率
+            #计算准确率并记录
             acc = (logits.argmax(dim=-1) == labels.to(device)).float().mean()
-            # 记录.
             valid_loss.append(loss.item())
             valid_accs.append(acc)
-        # 求平均loss与平均准确率
+
+        #求平均损失与准确率
         valid_loss = sum(valid_loss) / len(valid_loss)
         valid_acc = sum(valid_accs) / len(valid_accs)
         # 更新日志
-        print(f"[ Valid | {epoch + 1:03d}/{n_epochs:03d} ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f}",end="")
-        if valid_acc > best_acc:
-            print(" -> best") 
-        else:
-            print("")
+        print(f"[ Valid | {epoch + 1:03d}/{n_epochs:03d} ] loss = {valid_loss:.5f}, acc = {valid_acc:.5f}" + (" ->best " if (valid_acc>best_acc) else "") )
 
-        with open(f"./{_exp_name}_log.txt","a") as op:
-            op.write(f"{valid_loss:.5f} {valid_acc:.5f}\n")
+        if log_open:
+            with open(logfile,"a") as op:
+                op.write(f"{epoch + 1:03d} {train_loss:.5f} {train_acc:.5f} {valid_loss:.5f} {valid_acc:.5f}\n")
    
         # 保存模型
         if valid_acc > best_acc:
             print(f"Best model found at epoch {epoch}, saving model")
-            torch.save(model.state_dict(), f"{_exp_name}.pth") # only save best to prevent output memory exceed error
+            torch.save(model.state_dict(), modelfile) # only save best to prevent output memory exceed error
             best_acc = valid_acc
             stale = 0
-            with open(f"./{_exp_name}_test_log.txt","a") as op:\
-                op.write(f"{epoch + 1:03d} {Testing():.5f}\n")
+
+            if log_open:
+                with open(scorefile,"a") as op:
+                    op.write(f"{epoch + 1:03d} {Testing():.5f}\n")
         else:
             stale += 1
             if stale > patience:
@@ -229,5 +224,4 @@ if __name__=="__main__":
                 break
 
     print("final test acc = ",Testing())
-    # 如果已经训练完成, 则直接读取网络参数. 注意文件名改为自己的信息
     
